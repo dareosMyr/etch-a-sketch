a015f3b37bae54a3aeef81fc2635aad440d87170
Dareos Meyer <dareosmyr@gmail.com>
2019-02-22 13:40:52 -0800

    Code to create n-sized grid in place.

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const container = document.querySelector('#container');
+
+let side = 16;
+let items = side * side;
+for (let i = 0; i < items; i++){
+    const divs = document.createElement('div');
+    divs.style.width = 960/side + 'px';
+    divs.style.outline = '1px solid black';
+    container.appendChild(divs);
+}
