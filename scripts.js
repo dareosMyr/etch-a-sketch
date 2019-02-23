9cab887442659e37c81fe8a2a887827bc92176d8
Dareos Meyer <dareosmyr@gmail.com>
2019-02-22 17:06:24 -0800

    Grid items now go to black on hover.

--- scripts.js
+++ scripts.js
@@ -2,9 +2,13 @@ const container = document.querySelector('#container');
 
 let side = 16;
 let items = side * side;
+
 for (let i = 0; i < items; i++){
     const divs = document.createElement('div');
     divs.style.width = 960/side + 'px';
-    divs.style.outline = '1px solid black';
+    // divs.style.outline = '1px solid black';
+    divs.addEventListener('mouseover', () => {
+        divs.classList.add('color');
+    });
     container.appendChild(divs);
 }
